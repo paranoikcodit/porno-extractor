643a42219307a749e210c658993be984a73e6584
paranoikcodit <paranoikpidoras@gmail.com>
2023-12-16 23:35:51 +0300

    Implement enum_dispatch logic for extractors

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,16 @@ pub use pornhub_extractor;
 pub use xvideos_extractor;
 
 #[enum_dispatch::enum_dispatch(Extractor)]
-#[derive(Clone)]
 pub enum Extractors {
     XVideos(xvideos_extractor::XvideoExtractor),
     Pornhub(pornhub_extractor::PornhubExtractor),
+}
+
+impl Clone for Extractors {
+    fn clone(&self) -> Self {
+        match self {
+            Extractors::Pornhub(x) => Extractors::Pornhub(x.clone()),
+            Extractors::XVideos(x) => Extractors::XVideos(x.clone()),
+        }
+    }
 }
