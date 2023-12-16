4eea40ab987fc4b53ada53e9b52d57158fab804a
paranoikcodit <paranoikpidoras@gmail.com>
2023-12-16 22:20:52 +0300

    Made pornhub struct with need to todo

--- crates/pornhub-extractor/src/lib.rs
+++ crates/pornhub-extractor/src/lib.rs
@@ -1,14 +1,40 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use extractor_core::Extractor;
+
+#[derive(Default)]
+pub struct Pornhub {
+    pub client: reqwest::Client,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Pornhub {}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Extractor for Pornhub {
+    async fn get_search_variants(
+        &self,
+        query: &str,
+    ) -> anyhow::Result<extractor_core::SearchResult> {
+        todo!()
+    }
+
+    async fn get_recommendations(
+        &self,
+        video_id: &str,
+    ) -> anyhow::Result<Vec<extractor_core::VideoPreview>> {
+        todo!()
+    }
+
+    async fn get_video(&self, video_id: &str) -> anyhow::Result<extractor_core::Video> {
+        todo!()
+    }
+
+    async fn get_videos_page(
+        &self,
+        page: u16,
+        page_type: extractor_core::PageType,
+    ) -> anyhow::Result<Vec<extractor_core::VideoPreview>> {
+        todo!()
+    }
+
+    async fn get_model(&self, model: &str) -> anyhow::Result<extractor_core::Model> {
+        todo!()
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub use extractor_core::{Extractor, Model, PageType, SearchResult, Video, VideoPreview};
+pub use pornhub_extractor;
 pub use xvideos_extractor;
